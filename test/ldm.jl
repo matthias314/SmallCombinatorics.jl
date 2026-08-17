8d2589d7207c99869a089638b6d8b141c32a6524
Simen Gaure <simen@gaure.no>
2026-08-17 09:59:41 +0200

    updated some tests

--- test/ldm.jl
+++ test/ldm.jl
@@ -1,14 +1,17 @@
 @testset "ldm" begin
-    for n in (1,2,6,8,48,120,1243)
+    for n in (1,2,6,8,14,31,48)
         s = rand(n)
-        for k in (2,3,4)
-            ldm(s, Val(k))
+        sv = FixedVector{n,eltype(s)}(s)
+        for k in 1:4
+            @inferred ldm(s, Val(k), UInt64)
+            @inferred ldm(sv, Val(k))
         end
+        @inferred ldm(sv)
     end
 
-    @test ldm(1:11).subsetsums === (33, 33)
-    @test ldm(1:11, Val(4)).subsetsums === (18, 17, 16, 15)
-    @test ldm(1:11, Val(4), UInt16).subsetsums === (18, 17, 16, 15)
-    @test ldm(1:11, Val(4), NoSubsets).subsetsums === (18, 17, 16, 15)
+    @test ldm(1:15).subsetsums === (60,60)
+    @test ldm(1:15, Val(5)).subsetsums === (25, 25, 24, 23, 23)
+    @test ldm(1:15, Val(5), UInt16).subsetsums === (25, 25, 24, 23, 23)
+    @test ldm(1:15, Val(5), NoSubsets).subsetsums === (25, 25, 24, 23, 23)
     @test ldm(exp.(range(0,log(10),length=20)), Val(3)).value ≈ 0.1456806569078246
 end
